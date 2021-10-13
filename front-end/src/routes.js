3881b13036b42dec370e85fa6b92aced9eb9e403
dylancolonhesi <dylan.reboucas@bandtec.com.br>
2021-10-13 14:18:07 -0300

    feat: add cadastro react

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import AlterarSenha from './pages/alterar-senha'
 import RecuperarSenha from './pages/recuperar-senha';
 import EsqueceuSenha from './pages/esqueceu-senha';
+import Cadastro from './pages/Cadastro'
 
 
 function Routes(){
@@ -10,7 +11,7 @@ function Routes(){
         <Router>
             <Switch>
                 <Route exact path="/login" component={AlterarSenha}/>
-                <Route exact path="/cadastro" component={AlterarSenha}/>
+                <Route exact path="/cadastro" component={Cadastro}/>
                 <Route exact path="/recuperar-senha" component={RecuperarSenha}/>
                 <Route exact path="/esqueceu-senha" component={EsqueceuSenha}/>
                 <Route exact path="/alterar-senha" component={AlterarSenha}/>
