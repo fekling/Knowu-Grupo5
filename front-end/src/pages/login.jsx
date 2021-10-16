a93901eed653e197ba54acff5a8753dd008c31c8
fekling <felipe.kling@bandtec.com.br>
2021-10-16 10:36:40 -0300

    feat: criacao componente axios

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -5,48 +5,61 @@ import Apple from "../assets/images/apple.png";
 import Facebook from "../assets/images/face.png";
 import Google from "../assets/images/Google.png";
 import Localizacao from "../assets/images/localizacao.png";
-import Bg from "../assets/images/bg-color.png"
+import Bg from "../assets/images/bg-color.png";
+import Axios from "../components/Axios";
+import api from '../../../back-end/Java/knowu-project/src/main/java/com/example/knowuproject/controle';
+
+
 
 function Index() {
-  return (
+
+    const [login, setLogin] = useState("david.mariano@bandtec.com.br");
+    const [password, setPassword] = useState("CharlieBrownJr");
+    const [erroAutentication, setErrorAutenticatin] = useState(false);
+
+    
+    return (
+
+    
+
     <div className="content-login">
         <div className="desktop">
-      
+
             <div className="header-desktop">
-            <img src={Logo} alt="" className="logo-desktop" />
-            <ul>
-               <a href=""> <li>Home</li></a>
-               <a href=""> <li>Sobre nós</li></a>
-               <a href=""> <li>Contato</li></a>
-            </ul>
-            <button className="cadastre">Cadastre-se</button>
+                <img src={Logo} alt="" className="logo-desktop" />
+                <ul>
+                    <a href=""> <li>Home</li></a>
+                    <a href=""> <li>Sobre nós</li></a>
+                    <a href=""> <li>Contato</li></a>
+                </ul>
+                <button className="cadastre">Cadastre-se</button>
             </div>
-            
+
             <div className="content-desktop">
                 <div className="esquerda">
-                <img src={Bg} alt="" className="bg-color-login" />
+                    <img src={Bg} alt="" className="bg-color-login" />
                     <h1>Entre na sua conta e divulgue seus eventos.</h1>
                     <img src={Localizacao} alt="" className="map" />
                 </div>
                 <div className="direita">
                     <form action="">
-                <input type="text" className="input-login" placeholder="usuário ou email"/>
-                <input type="text" className="input-login" placeholder="Senha"/>
-                <button className="button-login-desktop">Login</button>
-                </form>
-                <h2>Ou continue com</h2>
-            <div className="opcoes">
-                <div className="apple">
-                    <img src={Apple} alt="" className="apple-logo" />
-                </div>
-                <div className="google">
-                    <img src={Google} alt="" className="google-logo" />
-                </div>
-                <div className="facebook">
-                    <img src={Facebook} alt="" className="facebook-logo" />
-                </div>
-                </div>
-                <h2><u>Esqueceu sua senha</u></h2>
+                        <input type="text" className="input-login" placeholder="usuário ou email" />
+                        <input type="text" className="input-login" placeholder="Senha" />
+                        <button className="button-login-desktop " onClick={handleLogin}>Login</button>
+                    </form>
+                    <h2>Ou continue com</h2>
+                    <div className="opcoes">
+                        <div className="apple">
+                            <img src={Apple} alt="" className="apple-logo" />
+                        </div>
+                        <div className="google">
+                            <img src={Google} alt="" className="google-logo" />
+                        </div>
+                        <div className="facebook">
+                            <img src={Facebook} alt="" className="facebook-logo" />
+                        </div>
+                    </div>
+                    <h2><u>Esqueceu sua senha</u></h2>
                 </div>
             </div>
         </div>
@@ -57,27 +70,45 @@ function Index() {
             </div>
             <div className="content-mobile">
                 <h1>Entre na sua conta e divulgue seus eventos.</h1>
-                <input type="text" className="input-login" placeholder="usuário ou email"/>
-                <input type="text" className="input-login" placeholder="Senha"/>
+                <input type="text" className="input-login" placeholder="usuário ou email" />
+                <input type="text" className="input-login" placeholder="Senha" />
                 <button className="button-login">Login</button>
                 <h2>Ou continue com</h2>
-            <div className="opcoes">
-                <div className="apple">
-                    <img src={Apple} alt="" className="apple-logo" />
+                <div className="opcoes">
+                    <div className="apple">
+                        <img src={Apple} alt="" className="apple-logo" />
+                    </div>
+                    <div className="google">
+                        <img src={Google} alt="" className="google-logo" />
+                    </div>
+                    <div className="facebook">
+                        <img src={Facebook} alt="" className="facebook-logo" />
+                    </div>
                 </div>
-                <div className="google">
-                    <img src={Google} alt="" className="google-logo" />
-                </div>
-                <div className="facebook">
-                    <img src={Facebook} alt="" className="facebook-logo" />
-                </div>
-            </div>
 
-            <h2><u>Esqueceu sua senha</u></h2>
+                <h2><u>Esqueceu sua senha</u></h2>
             </div>
         </div>
     </div>
-  );
+    );
+
+    async function handleLogin() {
+        try {
+
+            const { data } = await api.get('/usuarios/login/${usuarioLogin}/${senha}');
+            console.log(data);
+            localStorage.setItem("@dataUser", JSON.stringify(data));
+            history.push("/Home");
+
+        } catch (err) {
+            setErrorAutenticatin(true);
+            console.log("usuario o usenha incorreto");
+        }
+
+
+    }
 }
+
+
 
 export default Index;
