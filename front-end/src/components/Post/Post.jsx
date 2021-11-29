befacc1e27dff930eaf0103961304ab0e38d5e41
FelipeMallasen <mallasenfelipe@gmail.com>
2021-11-29 11:41:53 -0300

    fix: css files

--- front-end/src/components/Post/Post.jsx
+++ front-end/src/components/Post/Post.jsx
@@ -12,7 +12,9 @@ function Post(props) {
           <img className="post-imagem" src={ImagemUsuario} alt="" />
           <h5 className="post-nome">AAAAAAAAAAA</h5>
           <h6 className="post-usuario">aaaaaaaaaaaa</h6>
-          <div className="post-corpo"></div>
+          <div className="post-corpo">
+              uhaudhuahudhauhduah auhudhauhduahudhau haudhauhduahudhau hdauhauhduhaudha
+          </div>
           <div className="post-container-btn">
               <button className="post-btn">Curtir</button>
               <button className="post-btn">Comentar</button>
