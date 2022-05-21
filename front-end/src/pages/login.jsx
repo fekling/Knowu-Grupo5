0cb5b14e4111e7e54d0125e3baf1ead38cbc40ab
Andre Santos <andredasil2002@gmail.com>
2022-05-21 18:55:35 -0300

    fix: login

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,132 +1,180 @@
-import React, { useState, useCallback } from 'react';
-import "../assets/css/login-react.css";
-import Logo from "../assets/images/logo.png";
-import Apple from "../assets/images/apple.png";
-import Facebook from "../assets/images/face.png";
-import Google from "../assets/images/Google.png";
-import Localizacao from "../assets/images/localizacao.png";
-import Bg from "../assets/images/bg-color.png";
+import React, { useState, useCallback } from "react";
+import "../assets/css/style.css";
+
+// Images
+import Logo from "../assets/img/logo.png";
+import OpenMenu from "../assets/img/open-menu.png";
+import CloseMenu from "../assets/img/close-menu.png";
+import Instagram from "../assets/img/Instagram.png";
+import Facebook from "../assets/img/Facebook.png";
+import Youtube from "../assets/img/Youtube.png";
+import Twitter from "../assets/img/Twitter.png";
+import Localizacao from "../assets/img/localizacao.png";
+
+// Api
+
 import Api from '../components/Axios';
+
+// Router
 import { useHistory } from "react-router-dom";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 function Index() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [erroAutentication, setErrorAutenticatin] = useState(false);
+  const history = useHistory();
 
-    const [login, setLogin] = useState("");
-    const [password, setPassword] = useState("");
-    const [erroAutentication, setErrorAutenticatin] = useState(false);
-    const history = useHistory();
+  const home = useCallback(async (dados) => {
+    dados.preventDefault();
+    window.location = `https://fekling.github.io/Knowu-Grupo5/front-end/src/assets/html/index.html`;
+  });
 
-    const home = useCallback(
-        async(dados) => {
-          dados.preventDefault();
-          window.location = `https://fekling.github.io/Knowu-Grupo5/front-end/src/assets/html/index.html`;
-        }
-      )
+  const sobre_nos = useCallback(async (dados2) => {
+    dados2.preventDefault();
+    window.location = `https://fekling.github.io/Knowu-Grupo5/front-end/src/assets/html/index.html#sobre_nos`;
+  });
 
-      const sobre_nos = useCallback(
-        async(dados2) => {
-          dados2.preventDefault();
-          window.location = `https://fekling.github.io/Knowu-Grupo5/front-end/src/assets/html/index.html#sobre_nos`;
-        }
-      )
-      
-      const contato = useCallback(
-        async(dados2) => {
-          dados2.preventDefault();
-          window.location = `https://fekling.github.io/Knowu-Grupo5/front-end/src/assets/html/index.html#contato`;
-        }
-      )
+  const contato = useCallback(async (dados2) => {
+    dados2.preventDefault();
+    window.location = `https://fekling.github.io/Knowu-Grupo5/front-end/src/assets/html/index.html#contato`;
+  });
 
-    
-    return (
+  return (
     <div className="content-login">
-        {/* { setErrorAutenticatin && (
-            <h1> erro de login ou senha</h1>
-        )
+      <nav id="navigation">
+        <div className="wrapper">
+          <img src={Logo} className="logo" />
+          <div className="menu">
+            <ul>
+              <li>
+                <a onClick={home}>Home</a>
+              </li>
+              <li>
+                <a onClick={sobre_nos}>Sobre nós</a>
+              </li>
+              <li>
+                <a onClick={contato}>Contato</a>
+              </li>
+            </ul>
+            <Link to={"/cadastro"}>
+              <a href="" className="button">
+                Cadastre-se
+              </a>
+            </Link>
 
-        } */}
-        <div className="desktop mobile">
-
-            <div className="header-desktop">
-                <img src={Logo} alt="" className="logo-desktop" />
-                <ul>
-                    <a onClick={home}> <li className="lista-login">Home</li></a>
-                    <a onClick={sobre_nos}> <li className="lista-login">Sobre nós</li></a>
-                    <a onClick={contato}> <li className="lista-login">Contato</li></a>
-                </ul>
-                <Link to={"/cadastro"}>
-                <button className="cadastre">Cadastre-se</button>
-                </Link>
-                
-            </div>
-
-            <div className="mobile">
-            <div className="header-mobile">
-                <img src={Logo} alt="" className="logo-mobile" />
-                <h1>Menu</h1>
-            </div>
+            <ul className="social-links">
+              <li>
+                <img
+                  src={Facebook}
+                  href="htps://facebook.com"
+                  target="_blank"
+                ></img>
+              </li>
+              <li>
+                <img
+                  src={Instagram}
+                  href="htps://instagram.com"
+                  target="_blank"
+                ></img>
+              </li>
+              <li>
+                <img
+                  src={Youtube}
+                  href="htps://youtube.com"
+                  target="_blank"
+                ></img>
+              </li>
+              <li>
+                <img
+                  src={Twitter}
+                  href="htps://twitter.com"
+                  target="_blank"
+                ></img>
+              </li>
+            </ul>
+          </div>
+          <button
+            className="open-menu"
+            aria-expanded="false"
+            onClick={openMenu}
+          >
+            <img src={OpenMenu} alt="" />
+          </button>
+          <button
+            className="close-menu"
+            aria-expanded="false"
+            onClick={closeMenu}
+          >
+            <img src={CloseMenu} alt="" />
+          </button>
         </div>
+      </nav>
 
-            <div className="content-desktop-login content-mobile-login">
-                <div className="esquerda">
-                    <img src={Bg} alt="" className="bg-color-login" />
-                    <h1 className="titulo-login">Entre na sua conta e divulgue seus eventos.</h1>
-                    <img src={Localizacao} alt="" className="map" />
-                </div>
-                <div className="direita">
-                    
-                        <input type="text" className="input-login" placeholder="usuário ou email" onChange={e => setLogin(e.target.value)}/>
-                        <input type="password" className="input-login" placeholder="Senha" onChange={e => setPassword(e.target.value)}/>
-                        <button className="button-login" onClick={handleLogin}>Login</button>
-                    
-                    <h2>Ou continue com</h2>
-                    <div className="opcoes">
-                        <div className="apple">
-                            <img src={Apple} alt="" className="apple-logo" />
-                        </div>
-                        <div className="google">
-                            <img src={Google} alt="" className="google-logo" />
-                        </div>
-                        <div className="facebook">
-                            <img src={Facebook} alt="" className="facebook-logo" />
-                        </div>
-                    </div>
-                    <Link to={"/esqueceu-senha"}>
-                    <h2><u>Esqueceu sua senha</u></h2>
-                    </Link>
-                </div>
+      <section id="login">
+        <div className="wrapper">
+          <div className="col-a">
+            <img src={Localizacao} alt="" />
+          </div>
+          <div className="col-b">
+            <div className="content">
+              <p>Entre na sua conta e divulgue seus eventos</p>
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Usuário ou e-mail"
+                onChange={(e) => setLogin(e.target.value)}
+              />
+              <input
+                type="password"
+                name=""
+                id=""
+                placeholder="Senha"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="button" onClick={handleLogin}>
+                Login
+              </button>
+              <Link to={"/esqueceu-senha"}>
+                <a href="">Esqueceu sua senha</a>
+              </Link>
             </div>
+          </div>
         </div>
+      </section>
     </div>
-    );
+  );
 
-    async function handleLogin() {
+  
+  async function handleLogin() {
 
-        try {
-            const { data } = await Api.post('/usuarios/login/',{
-                usuario: login,
-                senha: password
-            });
-            
-            console.log(data);  
-            var id = JSON.stringify(data);
-            // history.push('/home-evento')
-            window.location = `https://fekling.github.io/Knowu-Grupo5/?id=${id}`;
+    try {
+        const { data } = await Api.post('/usuarios/login/',{
+            usuario: login,
+            senha: password
+        });
+        
+        console.log(data);  
+        var id = JSON.stringify(data);
+        // history.push('/home-evento')
+        window.location = `https://fekling.github.io/Knowu-Grupo5/?id=${id}`;
 
-        } catch (err) {
-            setErrorAutenticatin(true);
-            console.log("usuario o usenha incorreto");
-        }
-
-
+    } catch (err) {
+        setErrorAutenticatin(true);
+        console.log("usuario o usenha incorreto");
     }
 
-    
+
+}
 }
 
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
 
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
 
 export default Index;
